2f320edfe929c5189b44c6520230b994a97a81ea
Daniel Marks <d@nielmarks.com>
2023-02-25 17:24:00 -0500

    first commit

---
+++ main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"time"
+
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/sparrc/go-ping"
+)
+
+var (
+	fqdn = []string{"google.com", "example.com", "github.com"}
+)
+
+func main() {
+	for {
+		for _, fqdn := range fqdn {
+			pingSuccess := prometheus.NewGauge(
+				prometheus.GaugeOpts{
+					Name:        "ping_success",
+					Help:        "Indicates if the ping was successful or not",
+					ConstLabels: prometheus.Labels{"fqdn": fqdn},
+				},
+			)
+			pingDuration := prometheus.NewGauge(
+				prometheus.GaugeOpts{
+					Name:        "ping_duration_seconds",
+					Help:        "Duration of the ping in seconds",
+					ConstLabels: prometheus.Labels{"fqdn": fqdn},
+				},
+			)
+
+			prometheus.MustRegister(pingSuccess)
+			prometheus.MustRegister(pingDuration)
+
+			pinger, err := ping.NewPinger(fqdn)
+			if err != nil {
+				pingSuccess.Set(0)
+				pingDuration.Set(0)
+				continue
+			}
+			pinger.OnRecv = func(pkt *ping.Packet) {
+				pingSuccess.Set(1)
+				pingDuration.Set(pkt.Rtt.Seconds())
+			}
+			pinger.OnFinish = func(stats *ping.Statistics) {
+				pingSuccess.Set(0)
+				pingDuration.Set(0)
+			}
+			pinger.Count = 1
+			pinger.Timeout = time.Second * 5
+			pinger.Run()
+		}
+		time.Sleep(time.Second * 30)
+	}
+}
